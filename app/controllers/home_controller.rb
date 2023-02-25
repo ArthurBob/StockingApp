aefac592f1f56d6529c876ee9bd801ea1382769b
Arthur Bobeuf <arthur.bobeuf@icloud.com>
2023-02-25 11:48:32 +0100

    installed devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
-  def index
-  end
+		def index
+				@api = StockQuote::Stock.new(api_key: 'pk_062031d20883444f9ea74e2610fe2011')
+				
+				if params[:ticker] == ""
+					@nothing = "Hey! You forgot to enter a symbol"
+				elsif params[:ticker]
+
+						@stock = StockQuote::Stock.quote(params[:ticker])
+				end
+		end
+
+		def about
+		end
 end
