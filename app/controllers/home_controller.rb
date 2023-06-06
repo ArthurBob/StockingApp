2890a674bde37448060bcc04e18ffb3b35258fd6
Arthur Bobeuf <arthur.bobeuf@icloud.com>
2023-06-06 13:08:10 +0200

    added stock table

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,10 @@
 class HomeController < ApplicationController
 		def index
 				@api = StockQuote::Stock.new(api_key: 'pk_062031d20883444f9ea74e2610fe2011')
-				
+
 				if params[:ticker] == ""
 					@nothing = "Hey! You forgot to enter a symbol"
 				elsif params[:ticker]
-
 						@stock = StockQuote::Stock.quote(params[:ticker])
 				end
 		end
